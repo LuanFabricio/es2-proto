6c5b5936bcb34e3bb050f07bec1f4d4ab8a80cc1
LuanFabricio <luanlimaleite@hotmail.com>
2023-12-12 20:53:19 -0300

    feat: Add a function to create city table from an array

--- cityTable.js
+++ cityTable.js
@@ -2,9 +2,9 @@ import { createTableHeader } from "./table.js";
 
 /**
 * @param {Map<string, {total: Notas, size: number[]}>} data 
-* @param {HTMLBodyElement} data 
+* @param {HTMLBodyElement} $body 
 * */
-export function createStateTable(data, $body) {
+export function createStateTableFromMap(data, $body) {
 	const $table = document.createElement("table");
 	$table.id = "state-table";
 
@@ -23,6 +23,59 @@ export function createStateTable(data, $body) {
 
 		const $thMunicipio = document.createElement("th");
 		$thMunicipio.innerText = key;
+		$row.appendChild($thMunicipio);
+
+		for (const key of columnGradesKeys) {
+			const $th = document.createElement("th");
+			$th.innerHTML = row.total[key];
+			$row.appendChild($th);
+		}
+
+		for (const grade of row.total.redacao.comps) {
+			const $th = document.createElement("th");
+			$th.innerText = grade;
+			$row.appendChild($th);
+		}
+
+		const $thRedacao = document.createElement("th");
+		$thRedacao.innerText = row.total.redacao.total;
+		$row.appendChild($thRedacao);
+
+		const $thGeral = document.createElement("th");
+		$thGeral.innerText = row.total.geral;
+		$row.appendChild($thGeral);
+
+		const $thPresenca1 = document.createElement("th");
+		$thPresenca1.innerText = row.size[0] + "%";
+		$row.appendChild($thPresenca1);
+
+		const $thPresenca2 = document.createElement("th");
+		$thPresenca2.innerText = row.size[1] + "%";
+		$row.appendChild($thPresenca2);
+
+		$table.appendChild($row);
+	}
+
+	$body.appendChild($table);
+}
+
+/**
+* @param {{city: string, total: Notas, size: number[]}[]} data 
+* @param {HTMLBodyElement} $body 
+* */
+export function createStateTableFromArray(data, $body) {
+	const $table = document.createElement("table");
+	$table.id = "state-table";
+
+	createTableHeader($table);
+	
+	const columnGradesKeys = ["cn", "ch", "lc", "mt"];
+
+	for (const row of data) {
+		const $row = document.createElement("tr");
+
+		const $thMunicipio = document.createElement("th");
+		$thMunicipio.innerText = row.city;
 		$row.appendChild($thMunicipio);
 
 		for (const key of columnGradesKeys) {
