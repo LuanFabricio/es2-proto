9da5c383da2cc96a4ece21ed5c3c33671727174b
LuanFabricio <luanlimaleite@hotmail.com>
2023-12-15 18:17:19 -0300

    fix: Fixing merge conflict in index.js

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import { createCityGradeFilter, createCityFilter, createDataGradeFilter, createD
 import createCityGradeSortBy, { cityGradeSortBy } from "./sort.js";
 import mapToArray from "./utils.js";
 import { addExportBtn } from "./export.js";
+import createCityGradeSortBy, { cityGradeSortBy } from "./sort.js";
+import mapToArray from "./utils.js";
 
 /**
 * @typedef {import("./database.js").Data} Data
